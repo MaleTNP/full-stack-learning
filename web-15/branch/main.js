24ef31cd175a9d31513b43dec7133def8db02394
Thanapong Yooyen <malethanapong@gmail.com>
2025-08-17 16:00:01 +0700

    add get-area function of web-15

--- web-15/branch/main.js
+++ web-15/branch/main.js
@@ -1,22 +1,34 @@
 
-import express from "express";
+var express = require("express");
 var daemon = express();
-daemon.listen(1500);
+daemon.listen(80);
 
-daemon.get("api/search", searchBranch);
+daemon.get("/api/search", searchBranch);
+daemon.get("/api/get-area", showArea);
 
 function searchBranch(request, response) {
   var city = request.query.city;
-  if (city == null) { city = "" };
+  if (city == null) { city = ""; }
   var flag = false;
   for (var i = 0; i < branches.length; i++) {
-    if (branches[i] == city) { flage == true; }
+    if (branches[i] == city) { flag = true; }
     if (branches[i] != city) { }
   }
   var result = { };
-  result.city = city;
+  result.city  = city;
   result.found = flag;
-  reponse.send(result);
+  response.send(result);
 }
 
 var branches = ["Atlanta", "Boston", "Chicago", "Detroit", "New York"];
+
+function showArea(request, response) {
+  var w = request.query.width;
+  var h = request.query.height;
+  var detail = { };
+  detail.width  = w;
+  detail.height = h;
+  detail.area   = w * h; 
+  detail.date   = new Date();
+  response.send(detail);
+}
