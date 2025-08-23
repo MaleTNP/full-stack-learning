1910ed73b3bddde19d858d3ac664b3f3f8d94d66
Thanapong Yooyen <malethanapong@gmail.com>
2025-08-23 22:04:46 +0700

    add web-18 web-22

---
+++ web-22/build.js
@@ -0,0 +1,23 @@
+var path = require("path")
+module.exports = { }
+module.exports.entry = path.resolve("code", "main.js")
+
+module.exports.output = { }
+module.exports.output.path = path.resolve("public")
+module.exports.output.filename = "final.js"
+
+module.exports.module = { }
+module.exports.module.rules = [
+	{
+		test: /js$/,
+		use: {
+			loader: "babel-loader",
+			options: {
+				presets: [
+				"@babel/preset-env", 
+				"@babel/preset-react"]
+			}
+		},
+		exclude: /node_modules/,
+	}
+]
---
+++ web-22/code/main.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+var t = document.getElementById("target")
+var r = ReactDOM.createRoot(t)
+r.render(<button>Love</button>)
